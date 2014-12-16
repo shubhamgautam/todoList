4ce2e885839af0028ceec1cce3ca3384e8cf7e3a
unknown <acharya.rajiv@gmail.com>
2014-12-16 16:41:10 -0500

    adding db

--- web.js
+++ web.js
@@ -1,25 +1,43 @@
 var express = require('express');
 var fs = require('fs');
+var redis = require('./redis_service').Redis;
+var client = new redis();
 var app = express.createServer(express.logger());
 var htmlfile = "index.html";
 var open = require('open');
-/*var path = require('path');
+var path = require('path');
 
 app.configure(function () {
-  app.use('/js', express.static(path.join(__dirname, '/js')));
-  app.use('/images', express.static(path.join(__dirname, '/images')));
-  app.use('/css', express.static(path.join(__dirname, '/css')));
-  app.use(express.bodyParser());
-  app.use(express.logger("short"));
-});*/
+	app.use('/js', express.static(path.join(__dirname, '/js')));
+	app.use(express.bodyParser());
+	app.use(express.logger("short"));
+});
+
+app.get('/redis/insert', function (request, response) {
+	client.insert(request.query.data, function (err, res) {
+		response.send(res);
+	});
+});
+
+app.get('/redis/getall', function (request, response) {
+	client.getall(function (err, res) {
+		response.send(res);
+	});
+});
+
+app.get('/redis/del', function (request, response) {
+	client.delete (request.query.id, function (err, res) {
+		response.send(res.toString());
+	});
+});
 
 app.get('/', function (request, response) {
-  var html = fs.readFileSync(htmlfile).toString();
-  response.send(html);
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
-  console.log("Listening on " + port);
-  open("http://localhost:8080");
+	console.log("Listening on " + port);
+	open("http://localhost:8080");
 });
